492510b5a28bb46bcf3a27a347f9b335511cfe76
LuG10 <145559732+LuG10@users.noreply.github.com>
2024-09-24 23:09:01 +0200

    Add files via upload

---
+++ app/loging.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> login </title> 
+ </head>
+	
+	
+<body>
+<form name="formulario">
+	Introduzca el nombre del usuario y su contraseña:
+	Nombre de usuario:<input type="text" name="nombreUsuario" value=""> 
+	Contraseña:<input type="text" name="contraseña" value=""> 
+  	
+	<br>
+	<input type="button" value="Acceder" onclick="comprobardatos()">
+</form>
+<html>
