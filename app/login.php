90967f08e9ad0752a3dd432dbefe0db2e8a026b2
carolismex1502 <carolismex1502@gmail.com>
2024-09-25 16:39:30 +0200

    estilo css

--- app/login.php
+++ app/login.php
@@ -1,16 +1,38 @@
 <html>
 <head>
 <title> login </title> 
+<link rel="stylesheet" href="estilo.css">
  </head>
 	
 	
 <body>
+ <header>
+        <h1>Mi sitio web</h1>
+       
+    </header>
+
+    <main>
+        <section>
+            <h2>Bienvenido</h2>  
+
+            <p>Este es el contenido principal de mi página.</p>
+        </section>
+    </main>
+
+    
+
 <form name="formulario">
 	Introduzca el nombre del usuario y su contraseña:
 	Nombre de usuario:<input type="text" name="nombreUsuario" value=""> 
 	Contraseña:<input type="text" name="contraseña" value=""> 
-  	
+  	<br>
+  
 	<br>
 	<input type="button" value="Acceder" onclick="comprobardatos()">
 </form>
+<footer>
+        <p>&copy; 2023 Mi sitio web</p>
+    </footer>
+ 
+</body>
 <html>
