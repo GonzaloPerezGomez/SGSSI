5f189320c45e74e788dad8c04fa777ed73e151dd
SilGiRamos <43036599+SilGiRamos@users.noreply.github.com>
2024-09-26 14:05:29 -0300

    Update login.php

--- app/login.php
+++ app/login.php
@@ -15,14 +15,14 @@
         <section>
             <h2>Bienvenido</h2>  
 
-            <p>Este es el contenido principal de mi página.</p>
+            <p align="center">Este es el contenido principal de mi página.</p>
         </section>
     </main>
 
     
 
 <form name="formulario">
-	<p>Introduzca el nombre del usuario y su contraseña:<P>
+	<p>Introduzca el nombre del usuario y su contraseña:</p>
 	Nombre de usuario:<input type="text" name="nombreUsuario" value=""> 
 	Contraseña:<input type="text" name="contraseña" value=""> 
   	<br>
