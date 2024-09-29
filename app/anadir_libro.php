74cd2792c151060834d0fc97f807dfa02552c996
Gonzalo <gonzaloperezgo@gmail.com>
2024-09-29 18:22:43 +0200

    añadir libro, editar libro

---
+++ app/anadir_libro.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title> Añadir libro </title>
+<link rel="stylesheet" href="estilo.css">
+</head>
+	
+	
+	<body>
+	<form name="datos_libro">
+    <p align="center">Introduzca la información pedida a continuación:</p>
+		Titulo:<br>
+		<input type="text" name="titulo"> 
+        Autor: <br>
+		<input type="text" name="autor"> <br>
+  		Fecha de Publicación:<br>
+  		<input type="text" name="f_publicacion" placeholder="AAAA-MM-DD"> <br>
+		ISBN:<br>
+		<input type="text" name="ISBN"><br>
+		Nº de Paginas:<br>
+		<input type="text" name="n_paginas"> <br>
+	</form>
+	
+	<div class="button-container">
+		<a class="button" onclick="">Añadir</a>
+		<a class="button" href="index.php">Cancelar</a>
+	</div>	
+	
+	<footer>
+        	<p align="center">&copy; 2023 Mi sitio web</p>
+	</footer>
+<html>
