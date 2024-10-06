093aebe7108bca701f6a2279655bfc5971522074
iratxurrutia <iratxeus2004@gmail.com>
2024-10-06 16:47:29 +0200

    mas libros

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: db:3306
--- Tiempo de generación: 26-09-2024 a las 22:38:48
+-- Tiempo de generación: 06-10-2024 a las 14:42:49
 -- Versión del servidor: 10.8.2-MariaDB-1:10.8.2+maria~focal
 -- Versión de PHP: 8.2.8
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `libro` (
-  `idLibro` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `idLibro` int(11) NOT NULL,
   `titulo` varchar(40) NOT NULL,
   `autor` varchar(20) NOT NULL,
   `f_publicacion` varchar(10) NOT NULL,
@@ -40,8 +40,10 @@ CREATE TABLE `libro` (
 -- Volcado de datos para la tabla `libro`
 --
 
-INSERT INTO `libro` (`titulo`, `autor`, `f_publicacion`, `ISBN`, `n_paginas`) VALUES
-('Nacidos de la Bruma', 'Brandon Sanderson', '2014-09-01', '123456789123', 500);
+INSERT INTO `libro` (`idLibro`, `titulo`, `autor`, `f_publicacion`, `ISBN`, `n_paginas`) VALUES
+(1, 'Nacidos de la Bruma', 'Brandon Sanderson', '2014-09-01', '123456789123', '500'),
+(8, 'Los Juegos del Hambre', 'Suzanne Collins', '2008-09-14', '9788413144856', '400'),
+(9, 'El Asesinato de Roger Ackroyd', 'Agatha Christie', '1926-06-12', '9788467052978', '272');
 
 -- --------------------------------------------------------
 
@@ -50,13 +52,13 @@ INSERT INTO `libro` (`titulo`, `autor`, `f_publicacion`, `ISBN`, `n_paginas`) VA
 --
 
 CREATE TABLE `usuarios` (
-  `idUsuario` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `idUsuario` int(11) NOT NULL,
   `nombre` text NOT NULL,
   `apellido` text NOT NULL,
   `numeroDNI` text NOT NULL,
   `letraDNI` text NOT NULL,
-  `telefono` text,
-  `nacimiento` text,
+  `telefono` text NOT NULL,
+  `nacimiento` text NOT NULL,
   `email` text NOT NULL,
   `usuario` text NOT NULL,
   `contrasena` text NOT NULL
@@ -66,8 +68,8 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`nombre`, `apellido`, `numeroDNI`, `letraDNI`, `telefono`, `nacimiento`, `email`, `usuario`, `contrasena`) VALUES ('admin', 'admin', '12345678', 'z', '123456789', '1800-01-01', 'admin@gmail.com', 'admin', '1234');
-
+INSERT INTO `usuarios` (`idUsuario`, `nombre`, `apellido`, `numeroDNI`, `letraDNI`, `telefono`, `nacimiento`, `email`, `usuario`, `contrasena`) VALUES
+(1, 'admin', 'admin', '12345678', 'z', '123456789', '1800-01-01', 'admin@gmail.com', 'admin', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -84,6 +86,22 @@ ALTER TABLE `libro`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libro`
+--
+ALTER TABLE `libro`
+  MODIFY `idLibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
