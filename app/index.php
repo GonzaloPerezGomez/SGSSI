1e1094910be60b5c69eee17a8d16044d73461e02
iratxurrutia <iratxeus2004@gmail.com>
2024-10-18 18:25:29 +0200

    Revert "Merge pull request #54 from GonzaloPerezGomez/mejoraL"
    This reverts commit b384d92e5b2f0b822bd3c521112e6b360568fbae, reversing
    changes made to 8d415eae62bae5d141303308fbe0e418bec5cc25.

--- app/index.php
+++ app/index.php
@@ -1,49 +1,33 @@
-
-<?php  
-//funcion que almacena la sesion iniciada en la web a lo largo de todo su funcionamiento
-session_start();?>
+<?php  session_start();?>
 <html>
 <head>
-
-<meta charset="UTF-8">
-<!-- titulo que se pondra en la pagina --> 
-<title> Página principal </title>
-<!-- indica desde que script modela la página web--> 
-<link rel="stylesheet" href="estilo.css">
+    <meta charset="UTF-8">
+    <title> Página principal </title>
+    <script src="comprobacionDeDatos.js"></script>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 
 <body>
-<!-- titulo que se pondra en la pagina --> 
-<h1>PÁGINA PRINCIPAL</h1>
+    <br><h1>PÁGINA PRINCIPAL</h1>
 
-<?php
-//si la sesión esta iniciada
-if (isset($_SESSION['user_id'])) { 
-    //se muestra
-    echo
-    '
-    //contenedor de botones con los siguientes botones
-    <div class="button-container">
-        // botón normal que al pulsar se redirige a la página items.php
-        <a class="button" href="items.php">Catálogo</a>
-        // botón normal que al pulsar redirige página a show_user.php 
-        <a class="button" href=show_user.php>
-        //se carga la imagen de una carpeta ubicada en el repositorio
-        <img src="image/user.png" style="height:20px;"></a>
-    </div>';}
-//si no ha iniciado sesión
-else {                              
-    echo
-    '
-    //contenedor de botones con los siguientes botones
-	<div class="button-container">
-        // botón normal que al pulsar se redirige a la página login.php
-        <a href="login.php" class="button">Iniciar Sesión</a>
-        // botón normal que al pulsar se redirige a la página register.php
-        <a href="register.php" class="button">Registrarse</a>
-        // botón normal que al pulsar se redirige a la página items.php
-        <a class="button" href="items.php">Catálogo</a>
-    </div>';}
+    <?php
+    if (isset($_SESSION['user_id'])) { //si ha iniciado sesión
+        echo
+        '
+        <div class="button-container">
+            <a class="button" href="items.php">Catálogo</a>
+            <a class="button" href=show_user.php>
+            <img src="image/user.png" style="height:20px;"></a>
+        </div>';}
+    else {                              //si no ha iniciado sesión
+        echo
+        '
+        <div class="button-container">
+            <a href="login.php" class="button">Iniciar Sesión</a>
+            <a href="register.php" class="button">Registrarse</a>
+            <a class="button" href="items.php">Catálogo</a>
+        </div>';}
+    ?>
 
 </body>
 </html>
