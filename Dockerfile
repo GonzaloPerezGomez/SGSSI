a3646920c89ac26861976084a0c07270d0870c11
carolismex1502 <carolismex1502@gmail.com>
2024-11-07 22:17:15 +0100

    Security

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM php:7.2.2-apache
+FROM php:8.3-apache-bookworm
 RUN mkdir /var/www/imagen /var/www/html/logs
 RUN chown -R www-data:www-data /var/www/imagen /var/www/html/logs
 RUN chmod -R 775 /var/www/imagen /var/www/html/logs
 COPY default_imagen/*.jpeg /var/www/imagen/
+
+COPY apache-config /etc/apache2/sites-available/000-default.conf 
+RUN a2enmod headers
+
+RUN echo "\nServerSignature Off\nServerTokens Prod\n" >> /etc/apache2/apache2.conf
+
 RUN docker-php-ext-install mysqli
--- app/login.php
+++ app/login.php
@@ -1,5 +1,11 @@
 <?php  
 //funcion que almacena la sesion iniciada en la web a lo largo de todo su funcionamiento
+session_set_cookie_params([
+    'lifetime' => 0,                // Session cookie (expires when the browser is closed)
+    'path' => '/',                  // Cookie is valid throughout the domain
+//    'secure' => true,               // Send cookie only over HTTPS connections
+    'httponly' => true              // Cookie is inaccessible to JavaScript
+]);
 session_start();
 
 // Rutas de los archivos de log
